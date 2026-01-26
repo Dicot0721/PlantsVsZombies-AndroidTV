1c64ae2c1d357f68972c84b1d03a63b52e50cdaf
Dicot0721 <149964080+Dicot0721@users.noreply.github.com>
2026-01-26 16:55:56 +0800

    chore: Code cleanup

--- app/src/main/cpp/PvZ/include/PvZ/SexyAppFramework/SexyApp.h
+++ app/src/main/cpp/PvZ/include/PvZ/SexyAppFramework/SexyApp.h
@@ -1,14 +1,28 @@
-//
-// Created by Administrator on 2026/1/15.
-//
+/*
+ * Copyright (C) 2023-2025  PvZ TV Touch Team
+ *
+ * This file is part of PlantsVsZombies-AndroidTV.
+ *
+ * PlantsVsZombies-AndroidTV is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * PlantsVsZombies-AndroidTV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * PlantsVsZombies-AndroidTV.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-#ifndef PLANTSVSZOMBIES_ANDROIDTV_SEXYAPP_H
-#define PLANTSVSZOMBIES_ANDROIDTV_SEXYAPP_H
-
+#ifndef PVZ_SEXYAPPFRAMEWORK_SEXYAPP_H
+#define PVZ_SEXYAPPFRAMEWORK_SEXYAPP_H
 
 #include "SexyAppBase.h"
-namespace Sexy {
 
+namespace Sexy {
 
 class __SexyApp : public __SexyAppBase {
 public:
@@ -20,4 +34,4 @@ public:
 
 } // namespace Sexy
 
-#endif // PLANTSVSZOMBIES_ANDROIDTV_SEXYAPP_H
+#endif // PVZ_SEXYAPPFRAMEWORK_SEXYAPP_H
