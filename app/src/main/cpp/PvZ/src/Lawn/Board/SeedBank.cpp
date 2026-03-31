17a4e8058d0428fb13ba04811d1bcb6a8f497548
GarlicImp <2863663269@qq.com>
2026-03-31 14:40:51 +0800

    fix: 修复 SeedBank::Draw 崩溃

--- app/src/main/cpp/PvZ/src/Lawn/Board/SeedBank.cpp
+++ app/src/main/cpp/PvZ/src/Lawn/Board/SeedBank.cpp
@@ -62,10 +62,12 @@ void SeedBank::Draw(Sexy::Graphics *g) {
     // (*(void (**)(uint32_t, int *, bool))(**((uint32_t **)g + 1) + 140))(*((uint32_t *)g + 1),transform,true);
     // }
 
-    VSSetupAddonWidget *addonWidget = mApp->mVSSetupMenu->mAddonWidget;
-    if (addonWidget && addonWidget->mBanMode) {
-        g->SetColorizeImages(true);
-        g->SetColor(Color(155, 155, 155));
+    if (mApp->mVSSetupMenu) {
+        VSSetupAddonWidget *addonWidget = mApp->mVSSetupMenu->mAddonWidget;
+        if (addonWidget && addonWidget->mBanMode) {
+            g->SetColorizeImages(true);
+            g->SetColor(Color(155, 155, 155));
+        }
     }
 
     if (mApp->mGameScene != GameScenes::SCENE_PLAYING) {
