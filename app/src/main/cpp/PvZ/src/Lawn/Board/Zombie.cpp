3c6457e6afd95b375feeff5c385470948814e2da
GarlicImp <2863663269@qq.com>
2026-05-06 19:30:06 +0800

    refactor: 取消联机同步蹦极被伞弹走

--- app/src/main/cpp/PvZ/src/Lawn/Board/Zombie.cpp
+++ app/src/main/cpp/PvZ/src/Lawn/Board/Zombie.cpp
@@ -4574,20 +4574,20 @@ void Zombie::BungeeLanding() {
 
     Plant *aPlant = mBoard->FindUmbrellaPlant(mTargetCol, mRow);
     if (aPlant) {
-        if (mApp->IsVSMode()) {
-            if (gTcpConnected) {
-                // Client waits for host-authoritative umbrella result; freeze descent to avoid falling out of map during high latency.
-                if (mAltitude < 0.0f) {
-                    mAltitude = 0.0f;
-                }
-                return;
-            }
-
-            if (gTcpClientSocket >= 0) {
-                U16U16_Event event = {{EventType::EVENT_SERVER_BOARD_ZOMBIE_BUNGEE_HIT_UMBRELLA}, uint16_t(mBoard->mZombies.DataArrayGetID(this)), uint16_t(mBoard->mPlants.DataArrayGetID(aPlant))};
-                netplay::PutEvent(event);
-            }
-        }
+        //        if (mApp->IsVSMode()) {
+        //            if (gTcpConnected) {
+        //                // Client waits for host-authoritative umbrella result; freeze descent to avoid falling out of map during high latency.
+        //                if (mAltitude < 0.0f) {
+        //                    mAltitude = 0.0f;
+        //                }
+        //                return;
+        //            }
+        //
+        //            if (gTcpClientSocket >= 0) {
+        //                U16U16_Event event = {{EventType::EVENT_SERVER_BOARD_ZOMBIE_BUNGEE_HIT_UMBRELLA}, uint16_t(mBoard->mZombies.DataArrayGetID(this)),
+        //                uint16_t(mBoard->mPlants.DataArrayGetID(aPlant))}; netplay::PutEvent(event);
+        //            }
+        //        }
 
         mApp->PlaySample(SOUND_BOING);
         mApp->PlayFoley(FoleyType::FOLEY_UMBRELLA);
